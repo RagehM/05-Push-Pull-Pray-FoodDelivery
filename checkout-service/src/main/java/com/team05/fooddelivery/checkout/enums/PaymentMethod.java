63f0647e532cc0b7a9132137e828b87482f427f9
RagehM <rageh205@gmail.com>
2026-04-01 01:46:06 +0200

    feat(checkout-service): created payment, offer and paymentOffer entities

---
+++ checkout-service/src/main/java/com/team05/fooddelivery/checkout/enums/PaymentMethod.java
@@ -0,0 +1,7 @@
+package com.team05.fooddelivery.checkout.enums;
+
+public enum PaymentMethod {
+    CREDIT_CARD,
+    CASH_ON_DELIVERY,
+    WALLET
+}
---
+++ checkout-service/src/main/java/com/team05/fooddelivery/checkout/model/Payment.java
@@ -0,0 +1,126 @@
+package com.team05.fooddelivery.checkout.model;
+
+import com.team05.fooddelivery.checkout.enums.PaymentMethod;
+import com.team05.fooddelivery.checkout.enums.PaymentStatus;
+import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Entity
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long orderId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, columnDefinition = "paymentmethod")
+    private PaymentMethod method;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, columnDefinition = "paymentstatus")
+    private PaymentStatus status;
+
+    @Column(columnDefinition = "jsonb")
+    @JdbcTypeCode(SqlTypes.JSON)
+    private Map<String, Object> transactionDetails = new HashMap<>();
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "payment", cascade = CascadeType.ALL)
+    private List<PaymentOffer> paymentOffers = new ArrayList<>();
+
+    // this was not specified in the requirements but i think it makes sense (should be checked)
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public PaymentMethod getMethod() {
+        return method;
+    }
+
+    public void setMethod(PaymentMethod method) {
+        this.method = method;
+    }
+
+    public PaymentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PaymentStatus status) {
+        this.status = status;
+    }
+
+    public Map<String, Object> getTransactionDetails() {
+        return transactionDetails;
+    }
+
+    public void setTransactionDetails(Map<String, Object> transactionDetails) {
+        this.transactionDetails = transactionDetails;
+    }
+
+    public List<PaymentOffer> getPaymentOffers() {
+        return paymentOffers;
+    }
+
+    public void setPaymentOffers(List<PaymentOffer> paymentOffers) {
+        this.paymentOffers = paymentOffers;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
