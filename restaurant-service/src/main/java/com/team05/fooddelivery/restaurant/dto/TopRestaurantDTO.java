7287a206ae383ee98552387bc2313e7c14962467
demiana <demianareffaat@gmail.com>
2026-04-10 01:15:05 +0200

    feat(restaurant-service): implement top rated restaurants report (55-15053)

---
+++ restaurant-service/src/main/java/com/team05/fooddelivery/restaurant/dto/TopRestaurantDTO.java
@@ -0,0 +1,12 @@
+package com.team05.fooddelivery.restaurant.dto;
+
+public record TopRestaurantDTO
+
+    ( Long restaurantId,
+     String name,
+     Double rating,
+     Long totalOrders)
+{
+
+
+    }
