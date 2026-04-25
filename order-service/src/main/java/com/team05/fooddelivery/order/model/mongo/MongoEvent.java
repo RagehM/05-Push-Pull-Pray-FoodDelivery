72efa0c6e9ee8d9006c7645ae3b382a83bbf40ba
Omar Riad <omarriad2006@gmail.com>
2026-04-25 10:46:01 +0300

    feat(order): configured mongo entities (55-16204)

---
+++ order-service/src/main/java/com/team05/fooddelivery/order/model/mongo/MongoEvent.java
@@ -0,0 +1,11 @@
+package com.team05.fooddelivery.order.model.mongo;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public interface MongoEvent {
+    String getId();
+    LocalDateTime getTimestamp();
+    String getAction();
+    Map<String, Object> getDetails();
+}
---
+++ order-service/src/main/java/com/team05/fooddelivery/order/model/mongo/OrderEvent.java
@@ -0,0 +1,55 @@
+package com.team05.fooddelivery.order.model.mongo;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("order_events")
+public class OrderEvent implements MongoEvent{
+    @Id
+    private String id;
+    @Indexed
+    private Long orderId;
+    private String action;
+    private LocalDateTime timestamp;
+    private Map<String, Object> details = new HashMap<String, Object>();
+
+    public OrderEvent() {
+    }
+
+    public OrderEvent(Long orderId, String action, LocalDateTime timestamp) {
+        this.orderId = orderId;
+        this.action = action;
+        this.timestamp = timestamp;
+    }
+
+    public String getId() { return id; }
+
+    public Long getOrderId() { return orderId; }
+    public void setOrderId(Long orderId) { this.orderId = orderId; }
+
+    public String getAction() { return action; }
+    public void setAction(String action) { this.action = action; }
+
+    public LocalDateTime getTimestamp() { return timestamp; }
+    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
+
+    public Map<String, Object> getDetails() { return details; }
+    public void setDetails(Map<String, Object> details) { this.details = details; }
+
+    public static final class OrderEventActions {
+        public static final String ANALYTICS_VIEWED = "ANALYTICS_VIEWED";
+        public static final String INTERACTION_RECORDED = "INTERACTION_RECORDED";
+        public static final String ORDER_CONFIRMED = "ORDER_CONFIRMED";
+        public static final String ORDER_DELIVERED = "ORDER_DELIVERED";
+        public static final String ORDER_CANCELLED = "ORDER_CANCELLED";
+        public static final String ITEMS_ADDED = "ITEMS_ADDED";
+        public static final String ORDER_CREATED = "ORDER_CREATED";
+        public static final String ORDER_DELETED = "ORDER_DELETED";
+
+    }
+}
