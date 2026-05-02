0c973a848ed5b45eedf69e728170e0ef6c12ff18
mohamedgamal6606 <mohamedgamal6606@gmail.com>
2026-05-02 17:46:03 +0300

    fix(user): update delivery addresses default when creating a new default address (55-12064)

--- user-service/src/main/java/com/team05/fooddelivery/user/repository/DeliveryAddressRepository.java
+++ user-service/src/main/java/com/team05/fooddelivery/user/repository/DeliveryAddressRepository.java
@@ -7,10 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DeliveryAddressRepository extends JpaRepository<DeliveryAddress, Long> {
     @Query("SELECT u FROM User u WHERE u.id = :id")
     Optional<User> findUserById(@Param("id") Long id);
+
+    List<DeliveryAddress> findAllByUserId(Long userId);
+
 }
