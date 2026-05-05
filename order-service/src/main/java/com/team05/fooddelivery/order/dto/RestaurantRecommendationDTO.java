8c009b7ba8bffd8407d4fe5b15a31699c135a064
Mariam <maruma1682003@gmail.com>
2026-05-05 16:18:36 +0300

    feat(order): implement S3-F12 restaurant recommendations endpoint (55-16508)

--- order-service/src/main/java/com/team05/fooddelivery/order/dto/RestaurantRecommendationDTO.java
+++ order-service/src/main/java/com/team05/fooddelivery/order/dto/RestaurantRecommendationDTO.java
@@ -4,44 +4,69 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class RestaurantRecommendationDTO {
-    private final Long id;
+
+    private final Long restaurantId;
     private final String name;
     private final String cuisineType;
     private final Long score;
 
-    private RestaurantRecommendationDTO(Builder builder) {
-        this.id = builder.id;
-        this.name = builder.name;
-        this.cuisineType = builder.cuisineType;
-        this.score = builder.score;
-    }
-
     @JsonCreator
-    private RestaurantRecommendationDTO(
-        @JsonProperty("id") Long id,
-        @JsonProperty("name") String name,
-        @JsonProperty("cuisineType") String cuisineType,
-        @JsonProperty("score") Long score
+    public RestaurantRecommendationDTO(
+            @JsonProperty("restaurantId") Long restaurantId,
+            @JsonProperty("name") String name,
+            @JsonProperty("cuisineType") String cuisineType,
+            @JsonProperty("score") Long score
     ) {
-        this.id = id;
+        this.restaurantId = restaurantId;
         this.name = name;
         this.cuisineType = cuisineType;
         this.score = score;
     }
 
-    public Long getId() { return id; }
-    public String getName() { return name; }
-    public String getCuisineType() { return cuisineType; }
-    public Long getScore() { return score; }
+    private RestaurantRecommendationDTO(Builder builder) {
+        this.restaurantId = builder.restaurantId;
+        this.name = builder.name;
+        this.cuisineType = builder.cuisineType;
+        this.score = builder.score;
+    }
+
+    public Long getRestaurantId() {
+        return restaurantId;
+    }
+
+    public Long getId() {
+        return restaurantId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCuisineType() {
+        return cuisineType;
+    }
+
+    public Long getScore() {
+        return score;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
 
     public static class Builder {
-        private Long id;
+        private Long restaurantId;
         private String name;
         private String cuisineType;
         private Long score;
 
+        public Builder restaurantId(Long restaurantId) {
+            this.restaurantId = restaurantId;
+            return this;
+        }
+
         public Builder id(Long id) {
-            this.id = id;
+            this.restaurantId = id;
             return this;
         }
 
