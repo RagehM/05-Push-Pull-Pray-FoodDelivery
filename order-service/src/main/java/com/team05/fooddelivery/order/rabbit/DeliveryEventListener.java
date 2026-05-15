923df65929b08c06af1d149ef5f09195e487b485
Omar Riad <omarriad2006@gmail.com>
2026-05-15 16:41:31 +0300

    feat(order): RabbitMQ producers and consumers setup (55-16204)

---
+++ order-service/src/main/java/com/team05/fooddelivery/order/rabbit/DeliveryEventListener.java
@@ -0,0 +1,23 @@
+package com.team05.fooddelivery.order.rabbit;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import com.team05.fooddelivery.contracts.events.DeliveryCreatedEvent;
+import com.team05.fooddelivery.order.service.OrderService;
+
+@Component
+@RabbitListener(queues = "order.saga-feedback")
+public class DeliveryEventListener {
+    private final OrderService orderService;
+
+    public DeliveryEventListener(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @RabbitHandler
+    public void handleDeliveryCreatedEvent(DeliveryCreatedEvent event) {
+        // orderService.handleDeliveryCreatedEvent(event);
+    }
+}
