2f5c7426f32de46eeb70c48ae816b965f0cdee82
helguindy <133847291+helguindy@users.noreply.github.com>
2026-05-17 00:01:27 +0300

    fix(restaurant): use specific Feign clients to avoid checkout-service URL error (55-15053)

--- restaurant-service/src/main/java/com/team05/fooddelivery/restaurant/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/com/team05/fooddelivery/restaurant/RestaurantServiceApplication.java
@@ -3,6 +3,8 @@ package com.team05.fooddelivery.restaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+
+import com.team05.fooddelivery.contracts.feign.OrderServiceClient;
 //s2-f11
 import com.team05.fooddelivery.restaurant.repository.elasticsearch.RestaurantSearchRepository;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,7 +14,7 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableCaching
 //s2-f11
 @EnableElasticsearchRepositories(basePackageClasses = RestaurantSearchRepository.class)
-@EnableFeignClients(basePackages = "com.team05.fooddelivery.contracts.feign")
+@EnableFeignClients(clients = {OrderServiceClient.class})
 public class RestaurantServiceApplication {
 
     public static void main(String[] args) {
