35be3e3de7e4b941882c3ea92ea6cf3664e5e2b6
itsXrgon <ali.koheil@gmail.com>
2026-05-16 18:21:09 +0300

    feat(delivery): S5 isolate database sources in delivery serivce (55-24778)

--- delivery-service/src/main/java/com/team05/fooddelivery/delivery/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/com/team05/fooddelivery/delivery/DeliveryServiceApplication.java
@@ -1,5 +1,6 @@
 package com.team05.fooddelivery.delivery;
 
+import com.team05.fooddelivery.contracts.feign.OrderServiceClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -7,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableCaching
-@EnableFeignClients
+@EnableFeignClients(clients = {OrderServiceClient.class})
 public class DeliveryServiceApplication {
 
     public static void main(String[] args) {
