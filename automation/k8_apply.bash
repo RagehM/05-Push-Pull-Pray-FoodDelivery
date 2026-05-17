e36ea78f00499397929c2df4d43179ef2e4b6df2
Omar Riad <omarriad2006@gmail.com>
2026-05-17 22:19:35 +0300

    feat(infra): gateway, user, order and checkout kubernetes running healthy (55-16204)

--- automation/k8_apply.bash
+++ automation/k8_apply.bash
@@ -1,9 +1,23 @@
+kubectl delete namespace monitoring
+kubectl apply -f ./k8s/namespaces/monitoring-namespace.yaml
+kubectl apply -f ./k8s/monitoring/prometheus/
+kubectl apply -f ./k8s/monitoring/loki/
+kubectl apply -f ./k8s/monitoring/grafana/
+kubectl wait --for=condition=ready pod -l app=loki -n monitoring --timeout=120s
+
 kubectl delete namespace talabat
 kubectl apply -f ./k8s/namespaces/namespace.yaml
 kubectl apply -f ./k8s/secrets/
 kubectl apply -f ./k8s/pvcs/
 kubectl apply -f ./k8s/statefulsets/
 kubectl wait --for=condition=ready pod -l app=order-postgres -n talabat --timeout=120s
+kubectl wait --for=condition=ready pod -l app=checkout-postgres -n talabat --timeout=120s
+kubectl wait --for=condition=ready pod -l app=restaurant-postgres -n talabat --timeout=120s
+kubectl wait --for=condition=ready pod -l app=user-postgres -n talabat --timeout=120s
+# kubectl wait --for=condition=ready pod -l app=delivery-postgres -n talabat --timeout=120s
+kubectl wait --for=condition=ready pod -l app=rabbitmq -n talabat --timeout=120s
+kubectl wait --for=condition=ready pod -l app=redis -n talabat --timeout=120s
+# kubectl wait --for=condition=ready pod -l app=cassandra -n talabat --timeout=120s
 kubectl apply -f ./k8s/configmaps/
 kubectl apply -f ./k8s/deployments/
 kubectl apply -f ./k8s/services/
