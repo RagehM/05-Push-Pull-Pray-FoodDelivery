13761c2f0d6a7b115678cb28390d0d4b477c7e8c
Omar Riad <omarriad2006@gmail.com>
2026-05-18 01:57:36 +0300

    feat(infra): all runs kube, except delivery (55-16204)

--- automation/k8_apply.bash
+++ automation/k8_apply.bash
@@ -14,13 +14,13 @@ kubectl wait --for=condition=ready pod -l app=order-postgres -n talabat --timeou
 kubectl wait --for=condition=ready pod -l app=checkout-postgres -n talabat --timeout=120s
 kubectl wait --for=condition=ready pod -l app=restaurant-postgres -n talabat --timeout=120s
 kubectl wait --for=condition=ready pod -l app=user-postgres -n talabat --timeout=120s
-# kubectl wait --for=condition=ready pod -l app=delivery-postgres -n talabat --timeout=120s
+kubectl wait --for=condition=ready pod -l app=delivery-postgres -n talabat --timeout=120s
 kubectl wait --for=condition=ready pod -l app=rabbitmq -n talabat --timeout=120s
 kubectl wait --for=condition=ready pod -l app=redis -n talabat --timeout=120s
-# kubectl wait --for=condition=ready pod -l app=cassandra -n talabat --timeout=120s
+kubectl wait --for=condition=ready pod -l app=elasticsearch -n talabat --timeout=120s
+kubectl wait --for=condition=ready pod -l app=cassandra -n talabat --timeout=120s
+
 kubectl apply -f ./k8s/configmaps/
 kubectl apply -f ./k8s/deployments/
 kubectl apply -f ./k8s/services/
 kubectl apply -f ./k8s/api-gateway/
-
-
