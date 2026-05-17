1b7cbf27ed344a12caa061e8a3c83f656a02a0c7
RagehM <rageh205@gmail.com>
2026-05-17 14:12:01 +0300

    feat(setup): tested logging in checkout (55-3936)

--- user-service/src/main/java/com/team05/fooddelivery/user/security/UserLoaderHandler.java
+++ user-service/src/main/java/com/team05/fooddelivery/user/security/UserLoaderHandler.java
@@ -23,13 +23,12 @@ public class UserLoaderHandler extends AuthHandler {
         if (username == null) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized (UserLoader): missing subject claim");
         }
-//        try {
+        try {
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-//            return nextHandler.handle(new AuthContext(ctx.request(), ctx.token(), userDetails, ctx.requiredRole()));
-//        } catch (UsernameNotFoundException e) {
-//            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized (UserLoader): user not found");
-//        }
-        return nextHandler.handle(new AuthContext(ctx.request(), ctx.token(), userDetails, ctx.requiredRole()));
+            return nextHandler.handle(new AuthContext(ctx.request(), ctx.token(), userDetails, ctx.requiredRole()));
+        } catch (UsernameNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized (UserLoader): user not found");
+        }
     }
 
 }
